37c8430837e4eb44f555913d277214c53e3c0242
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-04-15 12:36:01 +0600

    search

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config(); //import .env file
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config() //import .env file
-
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fzhcz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
   const productsCollection = client.db("emaJohnStore").collection("products");
   const ordersCollection = client.db("emaJohnStore").collection("orders");
   // perform actions on the collection object
-  
-  //post request handling 
-  app.post('/addProduct',(req,res)=>{
-      const product = req.body
-      productsCollection.insertOne(product)
-      .then(result =>{
-          console.log(result.insertedCount);
-          res.send(result.insertedCount)
-      })
-    
-  })
+
+  //post request handling
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    productsCollection.insertOne(product).then((result) => {
+      console.log(result.insertedCount);
+      res.send(result.insertedCount);
+    });
+  });
 
   //data lode from mongodb
-  app.get('/',(req,res) => {
-        res.send(`Welcome to Ema-John-Server
+  app.get("/", (req, res) => {
+    res.send(`Welcome to Ema-John-Server
         Author: Kazi MD Minhajul Haider
-        `)
-})
-  app.get('/products',(req,res) => {
-      productsCollection.find({}).toArray((err,documents)=>{
-          res.send(documents)
-      })
-  })
+        `);
+  });
+  app.get("/products", (req, res) => {
+    const search = req.query.search;
+    productsCollection
+      .find({ name: { $regex: search } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
 
-  app.get('/products/:key',(req,res) => {
-    productsCollection.find({key: req.params.key}).toArray((err,documents)=>{
-        res.send(documents[0])
-    })
-})
+  app.get("/products/:key", (req, res) => {
+    productsCollection
+      .find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
 
-  app.post('/productsByKeys',(req,res)=>{
-    const productKeys = req.body
-    productsCollection.find({key: { $in: productKeys}})
-    .toArray((err,documents)=>{
-      res.send(documents)
-    })
-  })
+  app.post("/productsByKeys", (req, res) => {
+    const productKeys = req.body;
+    productsCollection
+      .find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
 
-  app.post('/addOrder',(req,res)=>{
-    const order = req.body
-    ordersCollection.insertOne(order)
-    .then(result =>{
-        res.send(result.insertedCount > 0)
-    })
-  
-})
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
 });
 
-app.listen(process.env.PORT ||5000)
+app.listen(process.env.PORT || 5000);
